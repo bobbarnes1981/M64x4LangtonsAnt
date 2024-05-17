67c7f3df83dd06f1d52d98eb938230b6156c97b4
bobbarnes1981 <robertbarnes1981@googlemail.com>
2024-05-17 17:04:52 +0100

    double quote paths

--- init_lin.sh
+++ init_lin.sh
@@ -5,8 +5,8 @@ LOCATION="../Minimal-64x4-Home-Computer"
 
 # copy the emulator into the current directory
 # currently the emulator will need to be executed using wine
-cp $LOCATION/Revision 1.1/FLASH Images/flash.bin .
-cp $LOCATION/Support/Emulator/Minimal64x4.exe .
+cp "$LOCATION/Revision 1.1/FLASH Images/flash.bin" .
+cp "$LOCATION/Support/Emulator/Minimal64x4.exe" .
 
 # compile the assmebler into the current directory
 g++ $LOCATION/Support/Assembler/asm.cpp -O2 -o ./asm -s -static
