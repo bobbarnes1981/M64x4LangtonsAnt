2fb9554fcbe8dbd2711a86367ea8b442e49327e5
Robert Barnes <robertbarnes1981@googlemail.com>
2024-05-17 07:34:24 +0100

    Update init_lin.sh

--- init_lin.sh
+++ init_lin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# path to the Minimal 64x4 repository
 LOCATION="../Minimal-64x4-Home-Computer"
 
-#cp $LOCATION/Revision 1.1/FLASH Images/flash.bin .
-#cp $LOCATION/Support/Emulator/Minimal64x4.exe .
+# copy the emulator into the current directory
+# currently the emulator will need to be executed using wine
+cp $LOCATION/Revision 1.1/FLASH Images/flash.bin .
+cp $LOCATION/Support/Emulator/Minimal64x4.exe .
 
+# compile the assmebler into the current directory
 g++ $LOCATION/Support/Assembler/asm.cpp -O2 -o ./asm -s -static
