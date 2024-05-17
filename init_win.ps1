4e9585b9c39f81601d85fbbd500f57d30dc3fdbf
Robert Barnes <robertbarnes1981@googlemail.com>
2024-05-17 07:35:58 +0100

    Update init_win.ps1

--- init_win.ps1
+++ init_win.ps1
@@ -1,6 +1,10 @@
+
+# path to the Minimal 64x4 repository
 $location = "..\Minimal-64x4-Home-Computer"
 
+# copy the emulator into the current directory
 Copy-Item "$location\Revision 1.1\FLASH Images\flash.bin" .
 Copy-Item "$location\Support\Emulator\Minimal64x4.exe" .
 
+# copy the assembler into the current directory
 Copy-Item "$location\Support\Assembler\asm.exe" .
